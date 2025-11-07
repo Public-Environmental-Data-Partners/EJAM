c75124e57aa240d3dd91beedbd7f3a5f0d87d467
ejanalyst <ejam@ejanalysis.com>
2025-11-06 21:00:39 -0500

    Revert "Update ejam2tableviewer.R"
    This reverts commit 4635108de74c051135d04d817c8d6c9404ea4ab7.

--- R/ejam2tableviewer.R
+++ R/ejam2tableviewer.R
@@ -2,18 +2,20 @@
 #'
 #' @param out output of ejamit(), or one table like \code{ejamit()$results_overall},
 #'   or subset like \code{ejamit()$results_bysite[7,]}
-#' @param fname optional. path and filename of the html file to save the table to,
+#' @param filename optional. path and name of the html file to save the table to,
 #'   or it uses tempdir() if not specified. Set it to NULL to prevent saving a file.
 #' @param maxrows only load/ try to show this many rows max.
 #' @param launch_browser set TRUE to have it launch browser and show report.
-#'   Ignored if not interactive() or if fname is set to NULL.
+#'   Ignored if not interactive() or if filename is set to NULL.
+#' @param ... passed to [DT::datatable()]
+#'
 #' @return a datatable object using [DT::datatable()]
 #'   that can be printed to the console or shown in the RStudio viewer pane
 #' @examples ejam2tableviewer(testoutput_ejamit_10pts_1miles)
 #'
 #' @export
 #'
-ejam2tableviewer = function(out, fname = 'automatic', maxrows = 1000, launch_browser = TRUE) {
+ejam2tableviewer = function(out, filename = 'automatic', maxrows = 1000, launch_browser = TRUE, ...) {
 
   if (!interactive()) {launch_browser <- FALSE} # but that means other functions cannot override this while not interactive.
 
@@ -22,84 +24,147 @@ ejam2tableviewer = function(out, fname = 'automatic', maxrows = 1000, launch_bro
   } else {
     x <- out
   }
+  if ("sitetype" %in% names(out)) {sitetype <- out$sitetype} else {
+    sitetype <- sitetype_from_dt(x)
+  }
   if (!is.data.frame(x)) { # data.table is ok too
     stop("Input must be a data frame")
   }
 
+  # cap on # of rows! ####
   x <- x[1:min(nrow(x), maxrows), ]
 
+  # round/sigfig (again?) ####
   x <- table_signif_round_x100(x)
-  # x <- table_x100(x, cnames = names_pct_as_fraction_ejamit)
-  # x <- table_round(x)
+
+  x$pop <- prettyNum(round(x$pop), big.mark = ',')
+
+  # freeze "Site ID" in column 1 ####
+
+  n_cols_freeze <- 1
+  x <- dplyr::relocate(x, ejam_uniq_id) # 1st column
+  names(x) <- gsub("ejam_uniq_id", "Site ID", names(x))
+
+  # DT::datatable() options ####
 
   dt <-    DT::datatable(x,
-                         # colnames = out$longnames,
+
+                         ## fixcolnames ####
                          colnames = fixcolnames(names(x), 'r', 'long'),
                          rownames = FALSE,
-                         escape = FALSE,  # related to showing URLs correctly but note security implication
-                         caption = "", # paste0(nrow(x),  ' SITES \\"' , ' ', '\\"'),
-                         filter = "top")
+
+                         ## column filters ####
+                         filter = 'top',
+
+                         ## selecting rows
+                         ## to allow selection of one row at a time (remove to allow multiple)
+                         ###selection = 'single',
+                         selection = 'none',
+
+                         ## SCROLL RIGHT/LEFT ##############
+
+                         extensions = c('FixedColumns'),
+
+                         options = list(
+                           fixedColumns = list(leftColumns = n_cols_freeze),
+                           scrollX = TRUE,
+
+                           ## column width ####
+                           autoWidth = TRUE,
+
+                           ## SCROLL UP/DOWN  ##############
+
+                           ## freeze header row when scrolling down
+                           fixedHeader = TRUE,
+                           pageLength = 100,
+                           ## set scroll height up and down
+                           scrollY = '500px',
+
+                           ## remove global search box ####
+                           dom = 'lrtip'
+                         ),
+                         ## table height ####
+                         height = 1500,
+
+                         ## > *** not escape HTML??? for links ####
+                         escape = FALSE,  # *** escape = FALSE may add security issue but makes links clickable in table
+
+                         ...) %>%
+    DT::formatStyle(names(x), 'white-space' = 'nowrap')
+
   ################################# #
-
-  # if fname was missing, then create a name and save it in temp dir.
-  # if fname was set to some path by user, then save it there not in temp dir
-  # if fname was set to NULL by user, then do not save, and cannot see in browser
-
+  # . ####
+  # save file if not in shiny web app ####
+  #
+  # if filename was missing, then create a name and save it in temp dir.
+  # if filename was set to some path by user, then save it there not in temp dir
+  # if filename was set to NULL by user, then do not save, and cannot see in browser
+  #
   # For now at least, do not try to save file if in shiny app!
   if (!shiny::isRunning()) {
 
-    # save/try save if (fname missing) or (fname  provided and not null)
-    if (missing(fname)) {trysave <- TRUE} else  {if (!is.null(fname)) {trysave <- TRUE}}
+    # save/try save unless NULL was specified as a way to suppress save
+    if (!is.null(filename)) {trysave <- TRUE} else {trysave <- FALSE}
 
     if (trysave)  {
-    # (NULL would mean do not save and do not browse)
+      # (NULL would mean do not save and do not browse)
 
-    # Validate folder and or file
+      # Validate folder and or file
 
-    validfoldernotfile = function(x) {x = file.info(x)$isdir; x[is.na(x)] <- FALSE; return(x)}
+      validfoldernotfile = dir.exists # function(x) {x = file.info(x)$isdir; x[is.na(x)] <- FALSE; return(x)}
       # BAD folder or missing param
-      if (missing(fname) ||
-          (  !validfoldernotfile(dirname(fname))) ) {
-        if ( !validfoldernotfile(dirname(fname))) {
-          warning("ignoring filename because path was invalid")
-        }
-        fname <- create_filename(ext = ".html", file_desc = "results_bysite", buffer_dist = x$radius.miles[1])
-        fname <- file.path(tempdir(), fname)
+      if (!validfoldernotfile(dirname(filename))) {
+        if (!missing(filename)) {
+          warning("ignoring filename because specified path was invalid")
+        } # else default automatic needs no warning
+
+        filename <- create_filename(ext = ".html", file_desc = "results_bysite", buffer_dist = x$radius.miles[1],
+                                    site_method = sitetype, with_datetime = TRUE)
+        filename <- file.path(tempdir(), filename)
       } else {
-        # good folder NOT WITH a filename, define a filename fname in that folder
-        if (validfoldernotfile(fname)) {
-          mydir = fname
-          fname <- create_filename(ext = ".html", file_desc = "results_bysite", buffer_dist = x$radius.miles[1])
-          fname = file.path(mydir, fname)
+        # good folder NOT WITH a filename, define a filename in that folder
+        if (validfoldernotfile(filename)) {
+          mydir = filename
+          filename <- create_filename(ext = ".html", file_desc = "results_bysite", buffer_dist = x$radius.miles[1],
+                                      site_method = sitetype, with_datetime = TRUE)
+          filename = file.path(mydir, filename)
         } else {
-          # good folder, WITH a filename w good extension, that may not yet exist?
-          if (validfoldernotfile(dirname(fname)) & tools::file_ext(fname) == "html") {
-            # all set
+          # use a default name and tempdir
+          if ("automatic" %in% filename || missing(filename)) {
+            filename <- create_filename(ext = ".html", file_desc = "results_bysite", buffer_dist = x$radius.miles[1],
+                                        site_method = sitetype, with_datetime = TRUE)
+            filename <- file.path(tempdir(), filename)
           } else {
-            # good folder, WITH BAD extension
-            if (validfoldernotfile(dirname(fname)) & !tools::file_ext(fname) == "html") {
-              warning("wrong extension, so adding .html")
-              fname = paste0(fname, ".html")
+            # good folder, WITH a filename w good extension, that may not yet exist?
+            if (validfoldernotfile(dirname(filename)) && tools::file_ext(filename) == "html") {
+              # all set
+            } else {
+              # good folder, WITH BAD extension
+              if (validfoldernotfile(dirname(filename)) && !tools::file_ext(filename) == "html") {
+                warning("wrong extension, so adding .html")
+                filename = paste0(filename, ".html")
+              }
             }
           }
         }
       }
       # save file:
-      htmlwidgets::saveWidget(dt, fname)
-      fname <- normalizePath(fname) # makes it work on Mac, e.g.
+      htmlwidgets::saveWidget(dt, filename)
+      filename <- normalizePath(filename) # makes it work on Mac, e.g.
       cat("\n")
-      cat("Interactive table of sites is saved here: ", fname, '\n')
-      cat(paste0("To open that folder: browseURL('", dirname(fname), "')\n"))
-      cat(paste0("To view that report in a browser: browseURL('", fname, "')\n"))
+      cat("Interactive table of sites is saved here: ", filename, '\n')
+      cat(paste0("To open that folder: browseURL('", dirname(filename), "')\n"))
+      cat(paste0("To view that report in a browser: browseURL('", filename, "')\n"))
 
       # maybe launch external browser to view table:
       if (!shiny::isRunning() && launch_browser) {
-        browseURL(fname)
+        browseURL(filename)
       }
     }
   }
 
-  ## shows table in the RStudio viewer pane by returning it (even if running shiny app locally - not sure what happens if on server)
+  ## shows table in the RStudio viewer pane by returning it
+  # (even if running shiny app locally - should work on server too)
   return(dt)
 }
 ################################################# #
