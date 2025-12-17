4a7f15dbb62c4331b6f0da4222a8cf21cfb9a1c6
ejanalysis <ejam@ejanalysis.com>
2025-12-17 10:12:33 -0500

    docs

--- R/app_ui.R
+++ R/app_ui.R
@@ -515,7 +515,7 @@ app_ui <- function(request) {
                                #includeCSS('inst/report/community_report/communityreport.css'),
                                #includeCSS('inst/report/community_report/main.css'),
                                ############################## #
-                               ###               > TABLES       ####
+                               ###               > HEADER AND TABLES       ####
                                uiOutput('comm_report_html'),
                                br(),
                                ############################## #
