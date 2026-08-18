90a40751548069fcfcc947a1fe112aa3d294b286
ejanalysis <ejam@ejanalysis.com>
2026-08-18 00:53:07 -0400

    set displayed API version to EJAM_VERSION
    need to ensure that EJAM_VERSION is actually set

--- inst/plumber/plumber.R
+++ inst/plumber/plumber.R
@@ -20,6 +20,12 @@
 #* @plumber
 function(pr) {
 
+  # Specify the EJAM version in the Swagger UI (otherwise it shows "0.0.0.9000" for dev builds).
+  plumber::pr_set_api_spec(pr, function(spec) {
+    spec$info$version <- Sys.getenv("EJAM_VERSION", unset = "development")
+    spec
+  })
+
   # Locate the two route files: prefer the source tree when running from a
   # checkout of the EJAM repo (so edits are picked up without reinstalling),
   # else use the installed package's copies.
